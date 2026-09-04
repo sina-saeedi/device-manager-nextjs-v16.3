2478ce35baa65d9153b08ec95965c52653e84510
Sina Saeedi <sina.sa.dev@gmail.com>
2026-09-04 20:58:12 +0330

    fix: close sidebar on mobile when user clicks on a link

--- components/app-sidebar/url-aware-menu.tsx
+++ components/app-sidebar/url-aware-menu.tsx
@@ -1,16 +1,23 @@
 "use client";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import {useSidebar} from "#/components/ui/sidebar";
 import {SidebarMenu, SidebarMenuButton, SidebarMenuItem} from "#/components/ui/sidebar";
 import {NAV_ITEMS} from "#/constants/navigation-items";
 
 export function URLAwareMenu() {
+  const {setOpenMobile} = useSidebar();
   const pathname = usePathname();
 
   return NAV_ITEMS.map((item) => {
     return (
       <SidebarMenu className="px-2" key={item.title}>
-        <SidebarMenuItem key={item.href}>
+        <SidebarMenuItem
+          key={item.href}
+          onClick={() => {
+            setOpenMobile(false);
+          }}
+        >
           <SidebarMenuButton render={<Link href={item.href} />} isActive={pathname === item.href}>
             <item.Icon />
             {item.title}
